2d776eb47c1211aaaeec221694ed84f53ed834b3
sue445 <sue445@sue445.net>
2018-10-20 19:48:38 +0900

    Impl TweetPixels#update_yesterday

---
+++ lib/tweet_pixles.rb
@@ -0,0 +1,46 @@
+require "pixela"
+require "active_support/all"
+
+require_relative "./twilog"
+
+module PixelaExt
+  refine(Pixela::Pixel) do
+    def create_or_update(quantity:)
+      create(quantity: quantity)
+    rescue Pixela::PixelaError => error
+      unless error.message.include?("This date pixel already exist")
+        raise
+      end
+
+      update(quantity: quantity)
+    end
+  end
+end
+
+class TweetPixels
+  attr_reader :twilog, :graph
+
+  using PixelaExt
+
+  def initialize(twitter_id:, pixela_username:, pixela_token:, pixela_graph_id:)
+    raise "pixela_username is required" unless pixela_username
+    raise "pixela_token is required"    unless pixela_token
+    raise "pixela_graph_id is required" unless pixela_graph_id
+
+    @twilog = Twilog.new(twitter_id)
+
+    client = Pixela::Client.new(username: pixela_username, token: pixela_token)
+    @graph = client.graph(pixela_graph_id)
+
+    Time.zone = "Tokyo"
+  end
+
+  def update_yesterday
+    update(Date.current - 1)
+  end
+
+  def update(date)
+    tweet = twilog.stat_tweets_count[date] || 0
+    graph.pixel(date).create_or_update(quantity: tweet)
+  end
+end
