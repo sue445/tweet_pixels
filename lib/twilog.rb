cb261c51b9c80dff56db401100a2685a1422e4e4
sue445 <sue445@sue445.net>
2024-03-30 14:59:59 +0900

    Use recent tweets instead of stat
    Now, stat doesn't contains all tweets count

--- lib/twilog.rb
+++ lib/twilog.rb
@@ -32,23 +32,15 @@ class Twilog
   end
 
   # @return [Hash<Date, Integer>]
-  def stat_tweets_count
-    html = URI.open("https://twilog.togetter.com/#{twitter_id}/stats", "User-Agent" => USER_AGENT).read
+  def recent_tweets_count
+    session.visit(twilog_url)
 
-    m1 = %r{ar_data\[1\]\s*=\s*\[(.+)\];}.match(html)
-    ar_data1 = m1.captures[0]
-    daily_tweets = ar_data1.split(",").map(&:to_i)
+    session.all("#side-recent ul li a").each_with_object({}) do |a, tweets|
+      m = /date-([0-9]{6})$/.match(a["href"])
+      date = Date.parse("20" + m[1])
+      count = a.find("span").text.to_i
 
-    m2 = %r{ar_lbl\[1\]\s*=\s*\[(.+)\];}.match(html)
-    ar_lbl1 = m2.captures[0]
-    dates = ar_lbl1.split(",").map { |s| Date.parse("20" + s.gsub("'", "")) }
-
-    unless daily_tweets.length == dates.length
-      raise "miss match array length (daily_tweets.length=#{daily_tweets.length}, dates.length=#{dates.length})"
-    end
-
-    dates.zip(daily_tweets).each_with_object({}) do |(date, tweet), hash|
-      hash[date] = tweet
+      tweets[date] = count
     end
   end
 
